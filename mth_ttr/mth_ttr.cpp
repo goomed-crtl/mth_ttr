d24713d548227fb32b36fdcd60d23ae6dc8aaeac
goomed-crtl <alohanickw@hotmail.com>
2025-03-28 10:58:27 -0400

    Add project files.

---
+++ mth_ttr/mth_ttr.cpp
@@ -0,0 +1,60 @@
+/*
+Title: Chapter 5 Exerise 17 - Math Tutor
+File Name: mth_ttr.cpp
+Programmer: Nicholas Woolston
+Date: 28/03/2025 (DD/MM/YYYY)
+Requirements: Write code that generates 2 random numbers to be added together. Display the numbers to the user and put a break in the code while the user
+gets the answer. Have the answer be displayed after the user presses button.
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <conio.h> 
+using namespace std;
+int GetRandNum1();
+int GetRandNum2();
+int AddBothNums(int num1, int num2);
+
+
+int main()
+{
+    char repeat;
+    do
+    {
+        srand(time(NULL));
+        int num1 = GetRandNum1();
+        int num2 = GetRandNum2();
+        int result = AddBothNums(num1, num2);
+
+        cout << "  " << num1 << endl << "+ " << num2 << "\n" << "----";
+        _getch(); // I had to look this one up. Originally i was just going to use cin to assign a value to nothing. 
+        //Essentially does the same thing, but isntead of waiting for 'enter' it's any key, and you dont assign a random value. 
+        cout << endl << setw(4) << result << endl;
+        cout << "Would you like to try another problem? (y/n) ";
+        
+        cin >> repeat; 
+        if (repeat == 'n')
+        {
+            cout << "Closing.";
+        }
+    } while (repeat == 'y');
+}
+
+int GetRandNum1()
+{
+    int num1 = rand() % 100;
+    return num1;
+}
+
+int GetRandNum2()
+{
+    int num2 = rand() % 100;
+    return num2;
+}
+
+int AddBothNums(int num1, int num2)
+{
+    int total = num1 + num2;
+    return total;
+}
